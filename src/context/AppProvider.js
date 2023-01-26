53fc186233911b446291449c69dd188d11c7c12a
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2023-01-26 17:57:06 -0300

    req 11

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -4,13 +4,17 @@ import { useState, useMemo, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import AppContext from './AppContext';
 import useHandleChange from '../hooks/useHandleChange';
+import useFetch from '../hooks/useFetch';
 
 function AppProvider({ children }) {
   const email = useHandleChange('');
   const password = useHandleChange('');
   const history = useHistory();
-
+  const ingredients = useHandleChange('');
   const [disabled, setDisabled] = useState(true);
+  const [ingredient, setIngredient] = useState('');
+  const searchHandleChange = useHandleChange('');
+  const { makeFetch } = useFetch();
 
   const validationError = () => {
     const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
@@ -21,6 +25,21 @@ function AppProvider({ children }) {
     } else {
       setDisabled(true);
     }
+  };
+  const fetchApi = async () => {
+    let url = '';
+    if (ingredients.value === 'ingredient') {
+      url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchHandleChange.value}`;
+    } else if (ingredients.value === 'name') {
+      url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchHandleChange.value}`;
+    } else {
+      url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchHandleChange.value}`;
+      if (searchHandleChange.value.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      }
+    }
+    const api = await makeFetch(url);
+    setIngredient(api);
   };
 
   const handleSubmit = () => {
@@ -35,12 +54,35 @@ function AppProvider({ children }) {
     validationError();
   }, [email, password]);
 
+  // useEffect(() => {
+  //   const urlName = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients.value}`;
+  //   const getByName = async () => {
+  //     const ApiNameRecipe = await makeFetch(urlName);
+  //     setIngredient(ApiNameRecipe);
+  //   };
+  //   getByName();
+  // }, [ingredients]);
+
   const values = useMemo(
     () => ({
-      email, password, disabled, handleSubmit,
+      email,
+      password,
+      disabled,
+      handleSubmit,
+      ingredients,
+      ingredient,
+      searchHandleChange,
+      fetchApi,
     }),
     [
-      email, password, disabled, handleSubmit,
+      email,
+      password,
+      disabled,
+      handleSubmit,
+      ingredients,
+      ingredient,
+      searchHandleChange,
+      fetchApi,
     ],
   );
 
